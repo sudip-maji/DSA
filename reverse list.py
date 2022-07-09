6074a9263e08e4cb7366692c36c6e50927bfced8
SUDIP_MAJI <91454674+sudip-maji@users.noreply.github.com>
2022-07-09 17:47:45 +0530

    Add files via upload

---
+++ reverse list.py
@@ -0,0 +1,14 @@
+# a=[1,2,3,4,5]
+# b=a[::-1]
+# print(b)
+# ans=[5,4,3,2,1]
+matrix=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+matrix.reverse()
+print(matrix)
+a=[1,2,3,4]
+a.reverse()
+print(a)
