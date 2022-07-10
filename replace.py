df554dcf27e4002116473d554673bf777e68bc8f
SUDIP_MAJI <91454674+sudip-maji@users.noreply.github.com>
2022-07-11 02:55:48 +0530

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,9 @@
+txt = "one one was a race horse, two two was one too."
+
+x = txt.replace("one", "three",2)
+
+print(x)
+z=txt.replace("two","ok")
+print(z)
+# tokens = ["2","1","+","3","*"]
+# tokens.replace("2")
