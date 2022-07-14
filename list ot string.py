1afa554b3f00519025434b5840c6096e3d9eaf6a
SUDIP_MAJI <91454674+sudip-maji@users.noreply.github.com>
2022-07-15 03:45:19 +0530

    Add files via upload

---
+++ list ot string.py
@@ -0,0 +1,18 @@
+a=[1,2,3,4,5]
+s="".join(str(i) for i in a)
+print(s)
+ans=int(s)+1
+print(ans)
+print(type(ans))
+
+
+# print(type(s))
+# x=int(s)
+# print(type(x))
+
+
+#  list of char to string
+b=['c','f']
+n="".join(b)
+print(n)
+# cf ans
