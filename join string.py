fc5a5f1858f5d7c8b4db032776979bfe4a84fdd5
SUDIP_MAJI <91454674+sudip-maji@users.noreply.github.com>
2022-07-17 03:22:04 +0530

    Add files via upload

---
+++ join string.py
@@ -0,0 +1,10 @@
+a="123"
+z=['5','6']
+b="->".join(a)
+# only can be done in strings and in lists of char
+print(b)
+print(type(b))
+q="->".join(z)
+# only can be done in strings and in lists of char
+print(q)
+print(type(q))
