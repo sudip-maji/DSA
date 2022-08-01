970b34d83a8051ef6892a64fd436c412f2001c0c
SUDIP_MAJI <91454674+sudip-maji@users.noreply.github.com>
2022-08-02 00:17:16 +0530

    Add files via upload

---
+++ divioion.py
@@ -0,0 +1,13 @@
+a=5
+b=-1
+c=2
+v=3
+print(round(a/v))
+# round to next integer if >0.5
+print(a/v)
+print(int(a/v))
+print(a//v)
+# // operator returns left value
+print(b//c)
+print(int(b/c))
+# int(a/v) pushes the value  towards 0
---
+++ division operators.py
@@ -0,0 +1,8 @@
+a=5
+print(a//2)
+print(a/3)
+print(round(a/3))
+print(-1//2)
+print(round(-1/2))
+print(-1//2)
+print(int(-1/2))
